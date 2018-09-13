f393f6bdbe2925f7ab52789d5d2d64cda3a4eb87
Janne Leppänen <janne@janneleppanen.com>
2018-09-13 23:47:27 +0300

    Add redux-actions and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import "antd/dist/antd.css";
 import { connect } from "react-redux";
+import { Layout, List, Checkbox, Button, Input } from "antd";
 
-import { addTask, removeTask } from "./Redux/TasksReducer";
+import { addTask, removeTask, toggleTask } from "./Redux/TasksReducer";
+
+const { Content } = Layout;
 
 interface State {
   newTask: string;
@@ -12,6 +15,7 @@ interface Props {
   tasks: Array<Task>;
   addTask(name: string): any;
   removeTask(name: string): any;
+  toggleTask(name: string): any;
 }
 
 class App extends React.Component<Props, State> {
@@ -20,34 +24,67 @@ class App extends React.Component<Props, State> {
   };
 
   public render() {
+    const tasks = this.props.tasks.filter((task: Task) => !task.done);
+    const archivedTasks = this.props.tasks.filter((task: Task) => task.done);
+
     return (
-      <div className="App">
+      <Content style={{ padding: "1rem", maxWidth: "600px", margin: "0 auto" }}>
         <h1>Todo App</h1>
 
         <h2>My Tasks</h2>
-        <form onSubmit={this.onSubmit}>
-          <input onChange={this.onTaskChange} value={this.state.newTask} />
-          <button type="submit">Add task</button>
+        <form
+          onSubmit={this.onSubmit}
+          style={{ marginBottom: "1rem", display: "flex" }}
+        >
+          <Input
+            onChange={this.onTaskChange}
+            value={this.state.newTask}
+            style={{ marginRight: ".5rem" }}
+          />
+
+          <Button htmlType="submit" type="primary">
+            Add task
+          </Button>
         </form>
 
-        <h2>Tasks</h2>
-        <ul>
-          {this.props.tasks.map((task: Task) => (
-            <li key={task.name} onClick={() => this.onTaskClick(task)}>
-              {task.name}
-            </li>
-          ))}
-        </ul>
-      </div>
+        {this.renderList("Tasks", tasks)}
+        {this.renderList("Done", archivedTasks)}
+      </Content>
     );
   }
+
+  renderList = (title: string, tasks: Array<Task>) => {
+    if (tasks.length === 0) return null;
+
+    return (
+      <React.Fragment>
+        <h2>{title}</h2>
+        <List
+          size="small"
+          dataSource={tasks}
+          renderItem={(task: Task) => (
+            <List.Item>
+              <label style={{ width: "100%" }}>
+                <Checkbox
+                  onChange={() => this.onTaskCheck(task)}
+                  checked={task.done}
+                  style={{ marginRight: ".5rem" }}
+                />
+                {task.name}
+              </label>
+            </List.Item>
+          )}
+        />
+      </React.Fragment>
+    );
+  };
 
   onTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({ newTask: e.target.value });
   };
 
-  onTaskClick = (task: Task) => {
-    this.props.removeTask(task.name);
+  onTaskCheck = (task: Task) => {
+    this.props.toggleTask(task.name);
   };
 
   onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -65,5 +102,5 @@ const mapStateToProps = (state: GlobalState) => {
 
 export default connect(
   mapStateToProps,
-  { addTask, removeTask }
+  { addTask, removeTask, toggleTask }
 )(App);
--- src/Redux/TasksReducer.ts
+++ src/Redux/TasksReducer.ts
@@ -1,47 +1,42 @@
+import { createActions, handleActions } from "redux-actions";
+
 export const ADD_TASK = "ADD_TASK";
 export const REMOVE_TASK = "REMOVE_TASK";
-
-export const addTask = (taskName: string) => ({
-  type: ADD_TASK,
-  payload: taskName
-});
-
-export const removeTask = (name: string) => ({
-  type: REMOVE_TASK,
-  payload: name
-});
+export const TOGGLE_TASK = "TOGGLE_TASK";
 
 const STATE: Array<Task> = [
   { name: "Read a book", done: false },
   { name: "Buy milk", done: false }
 ];
 
-export const TasksReducer = (state = STATE, action: any) => {
-  switch (action.type) {
-    case ADD_TASK:
-      return handleAddTask(state, action);
-    case REMOVE_TASK:
-      return handleRemoveTask(state, action);
-    default:
-      return state;
-  }
-};
-
-interface addTaskAction {
+interface basicAction {
   type: string;
   payload: string;
 }
 
-interface removeTaskAction {
-  type: string;
-  payload: string;
-}
+export const { addTask, removeTask, toggleTask } = createActions({
+  ADD_TASK: (taskName: string) => taskName,
+  REMOVE_TASK: (taskName: string) => taskName,
+  [TOGGLE_TASK]: (taskName: string) => taskName
+});
 
-const handleAddTask = (state: Array<Task>, action: addTaskAction) => {
-  const newTask = { name: action.payload, done: false };
-  return [...state, newTask];
-};
-
-const handleRemoveTask = (state: Array<Task>, action: removeTaskAction) => {
-  return state.filter((task: Task) => task.name !== action.payload);
-};
+export const TasksReducer = handleActions(
+  {
+    [addTask.toString()]: (state: Array<Task>, action: basicAction) => {
+      const newTask = { name: action.payload, done: false };
+      return [...state, newTask];
+    },
+    [removeTask.toString()]: (state: Array<Task>, action: basicAction) => {
+      return state.filter((task: Task) => task.name !== action.payload);
+    },
+    [TOGGLE_TASK]: (state: Array<Task>, action: basicAction) => {
+      return [...state].filter((task: Task) => {
+        if (task.name === action.payload) {
+          task.done = !task.done;
+        }
+        return task;
+      });
+    }
+  },
+  STATE
+);
